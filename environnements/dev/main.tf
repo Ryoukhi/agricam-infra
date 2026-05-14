d06caa3ba98e32bffdb18b599bdd4bc12660390e
Stephane <stephen.deutou@gmail.com>
2026-05-14 04:26:14 +0100

    ajustement terraform version

--- environnements/dev/main.tf
+++ environnements/dev/main.tf
@@ -2,6 +2,7 @@
 # Infrastructure AgriCam — Environnement de developpement
 # CamTech Solutions — Douala, Cameroun
 terraform {
+  required_version = ">= 1.5"
   backend "s3" {}
 
   required_providers {
